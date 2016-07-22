507cfc9fab4a00bd0ca4bb620d1eebab932de175
syndicatefx <syndicatefx@gmail.com>
2016-07-23 00:13:38 +0100

    hack away

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hey.js
-Completely customizable and dependency free Javascript dialogs
+
+Completely customizable and dependency free Javascript dialogs. An experiment built for future use, based on [this fiddle](https://jsfiddle.net/MrPolywhirl/feq6y8v4/).
+
+[Demo + info](http://syndicatefx.com/hey/)
+
+Not a finished product/solution, use at your own risk!
---
+++ hey.js
@@ -0,0 +1,108 @@
+/*  hey.js -  1.0.0-beta 
+ *
+ *  File: hey.js
+ *  Author: Paulo Nunes, http://syndicatefx.com
+ *  Source: https://github.com/syndicatefx/hey.js
+ *  License: MIT
+ */
+
+
+// Append dialog =====================================================================================
+
+function addHey() {
+
+  document.body.insertAdjacentHTML('afterbegin', '<div role="alertdialog" tabindex="0" aria-labelledby="hey-message" id="hey-overlay" class="hey-overlay"><div role="document" tabindex="0" id="hey-box" class="hey-box"><div id="hey-message" class="hey-box__message"></div><div id="hey-actions" class="hey-box__btn-group"></div></div></div>');
+  
+  // Set keyboard focus only on dialog
+  var dialog = document.getElementById("hey-box");
+  document.addEventListener("focus", function (event) {
+    if (!dialog.contains(event.target)) {
+      event.stopPropagation();
+      dialog.focus();
+    }
+  }, true);
+}
+
+// Remove dialog =====================================================================================
+
+function removeHey() {
+
+  var heyOverlay = document.getElementById('hey-overlay');
+  heyOverlay.parentNode.removeChild(heyOverlay);
+
+}
+
+// Remove dialog on ESC key press ====================================================================
+
+document.addEventListener("keydown", function (event) {
+
+  if (event.keyCode == 27) {
+    removeHey()
+  }
+
+});
+
+// Alert dialog ======================================================================================
+
+function heyAlert() {
+  this.show = function (dialog) {
+    addHey();
+
+    document.getElementById('hey-message').innerHTML = dialog;
+    document.getElementById('hey-actions').innerHTML = '<button class="hey-button" id="focused" onclick="heyAlert.ok()">OK</button>';
+    document.getElementById('focused').focus();
+  }
+  this.ok = function () {
+    removeHey();
+  }
+}
+
+var heyAlert = new heyAlert();
+
+// Confirm dialog =====================================================================================
+
+function heyConfirm() {
+  this.show = function (dialog, op) {
+    addHey();
+
+    document.getElementById('hey-message').innerHTML = dialog;
+    document.getElementById('hey-actions').innerHTML = '<button class="hey-button" onclick="heyConfirm.yes(\'' + op + '\')">Yes</button> <button class="hey-button" id="focused" onclick="heyConfirm.no()">No</button>';
+    document.getElementById('focused').focus();
+  }
+  this.no = function () {
+    removeHey();
+  }
+  this.yes = function (op) {
+    window[op]();
+    removeHey();
+  }
+}
+
+var heyConfirm = new heyConfirm();
+
+// Prompt dialog =======================================================================================
+
+function heyPrompt() {
+  this.show = function (dialog, func) {
+    addHey();
+
+    document.getElementById('hey-message').innerHTML = dialog;
+    document.getElementById('hey-message').innerHTML += '<input class="hey-input" id="heyPrompt-value">';
+    document.getElementById('hey-actions').innerHTML = '<button class="hey-button" onclick="heyPrompt.ok(\'' + func + '\')">OK</button> <button class="hey-button" onclick="heyPrompt.cancel()">Cancel</button>';
+    document.getElementById('heyPrompt-value').focus();
+  }
+  this.cancel = function () {
+    removeHey();
+  }
+  this.ok = function (func) {
+    var heyPromptValue = document.getElementById('heyPrompt-value').value;
+    if(heyPromptValue !== "") {
+      window[func](heyPromptValue);
+      removeHey();
+    } else {
+      document.getElementById('heyPrompt-value').classList.add("hey-input--required");
+    }
+  }
+}
+
+var heyPrompt = new heyPrompt();
